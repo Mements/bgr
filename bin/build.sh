5842d476268ea6835428a0228214a12c16e99a6f
Igor Berlenko <igorberlenko7@gmail.com>
2025-02-10 19:56:40 +0800

    feat: add cleanup commands and improve process deletion

--- bin/build.sh
+++ bin/build.sh
@@ -1,5 +1,5 @@
 bun build $(pwd)/src/watch.ts --compile --outfile $(pwd)/bin/bgwatch
 
-bun build $(pwd)/src/index.ts --compile --outfile $(pwd)/bin/bgrun
+bun build $(pwd)/src/index.ts --compile --outfile $(pwd)/bin/bgr
 
 export PATH="$(pwd):$PATH"
